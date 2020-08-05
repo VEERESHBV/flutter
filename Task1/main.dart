927aab559b39ea3d9572f5e99ff85727e10c4d5b
Veeresh B V <veereshbv04@gmail.com>
2020-08-05 08:19:33 +0530

    Create main.dart
    main function and stateless widget

---
+++ Task1/main.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+import "package:flutter/cupertino.dart";
+import 'home/ui.dart';
+
+main(){
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return myapp();
+  }
+
+}
